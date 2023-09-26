f98300c85bda6a21bc12008c536be153c548f5bc
GarbageAcc <a3026060@gmail.com>
2023-09-26 10:57:55 +0300

    Added init app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import NavRoutes from '../src/components/Routes';
+
+function App() {
+  return (
+    <div className="App">
+      <NavRoutes />
+    </div>
+  );
+}
+
+export default App;
